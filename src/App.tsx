4ae1f46758e31a651b575a1c3dbb26483da1a238
atik666 <faisal.atik@gmail.com>
2025-11-05 12:15:12 -0500

    Fix: Add basename to BrowserRouter for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/faculty-web-weaver">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/research" element={<Layout><Research /></Layout>} />
