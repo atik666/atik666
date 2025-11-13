55744401544d8de9cbf8fe10f1e49d0323c41048
atik666 <faisal.atik@gmail.com>
2025-11-13 15:45:08 -0500

    11/13

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/faculty-web-weaver/",
+  build: {
+    outDir: "docs", // Change output directory to docs
+  },
   server: {
     host: "::",
     port: 8080,
